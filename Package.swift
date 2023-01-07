aa8a17cef4e0bbc09aa1941368f7713b988a9d31
Darren Ehlers <me@darrenehlers.com>
2023-01-06 20:33:49 -0600

    Updated package dependencies

--- Package.swift
+++ Package.swift
@@ -28,8 +28,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/optonaut/ActiveLabel.swift", from: "1.1.5"),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.2.0"),
-        .package(url: "https://github.com/DoubleNodeOpen/AnimatedField.git", from: "2.5.29"),
-        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.10.5"),
+        .package(url: "https://github.com/DoubleNodeOpen/AnimatedField.git", from: "2.5.44"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.10.28"),
         .package(url: "https://github.com/SwipeCellKit/SwipeCellKit", from: "2.7.1")
     ],
     targets: [
