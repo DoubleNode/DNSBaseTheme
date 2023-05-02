999191ef67164793d181820d8950689fb4b5d448
Darren Ehlers <me@darrenehlers.com>
2023-05-02 15:57:58 -0500

    Added _baseStyle when remotely loading Themes

--- Sources/DNSBaseTheme/Theme/Styles/DNSThemeButtonStyle.swift
+++ Sources/DNSBaseTheme/Theme/Styles/DNSThemeButtonStyle.swift
@@ -37,8 +37,29 @@ open class DNSThemeButtonStyle: DNSThemeStyle {
     required public init() {
         super.init()
     }
-    required public init(styleName: String?, styleSetName: String? = "", backgroundColor: DNSUIColor = DNSUIColor.Base.background, border: DNSUIBorder = DNSUIBorder.Base.default, shadow: DNSUIShadow = DNSUIShadow.Base.default, skeletonable: DNSUIEnabled = DNSUIEnabled.Base.skeletonable, tintColor: DNSUIColor = DNSUIColor.Base.tint) {
-        fatalError("init(styleName:styleSetName:backgroundColor:border:shadow:skeletonable:tintColor:) has not been implemented")
+    required public init(styleName: String?,
+                         styleSetName: String? = "",
+                         backgroundColor: DNSUIColor = DNSUIColor.Base.Button.background,
+                         border: DNSUIBorder = DNSUIBorder.Base.button,
+                         shadow: DNSUIShadow = DNSUIShadow.Base.button,
+                         skeletonable: DNSUIEnabled = DNSUIEnabled.Base.Button.skeletonable,
+                         tintColor: DNSUIColor = DNSUIColor.Base.Button.tint) {
+        self.titleStyle = DNSThemeLabelStyle.Base.Button.title
+        self.subtitleStyle = DNSThemeLabelStyle.Base.Button.subtitle
+        super.init(styleName: styleName,
+                   styleSetName: styleSetName,
+                   backgroundColor: backgroundColor,
+                   border: border,
+                   shadow: shadow,
+                   skeletonable: skeletonable,
+                   tintColor: tintColor)
+        let name = self.name ?? "default"
+        DNSThemeButtonStyle.themeStyles[name] = self
+        let setName = self.setName ?? "Base"
+        if !setName.isEmpty {
+            let fullName = "\(setName).\(name)"
+            DNSThemeButtonStyle.themeStyles[fullName] = self
+        }
     }
     required public init(styleName: String?,
                          styleSetName: String? = "",
